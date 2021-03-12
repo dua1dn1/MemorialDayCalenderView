0dd0a950f09bf04e1c964ac3fedcc30324bf06c7
dua1dn1 <dua1dn1@naver.com>
2021-03-12 17:52:53 +0900

    start

---
+++ memorialdaycalenderview/src/components/loginForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export class loginForm extends React.Component {
+    render(){
+        return(
+        <form>
+            <lable>
+                ID : <input type= "text" name="id" />           
+            </lable> 
+            <label>
+                PW : <input type= "passwd" name="pw"/>
+            </label>
+            <button className="login">Login</button>
+            <button className="sign_up">SignUp</button>
+        </form>
+        );
+    }
+}
